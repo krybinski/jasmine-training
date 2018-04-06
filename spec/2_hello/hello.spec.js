404e16fbee2e4fd1a9544be3ca8ae9ecfeb666d4
Kamil Rybiński <kamil.rybinski@quantumlab.co>
2018-04-06 14:24:15 +0200

    Added new spec test for hello world

--- spec/2_hello/hello.spec.js
+++ spec/2_hello/hello.spec.js
@@ -1,8 +1,16 @@
 describe("Hello World", function() {
-	var HelloWorld = require('../../src/2_hello/HelloWorld');
+	var HelloWorld = require('../../src/2_hello/Hello');
+	var text;
+
+	beforeEach(function() {
+		text = HelloWorld();
+	});
 
 	it('should say Hello World!', function() {
-		const text = HelloWorld();
 		expect(text).toEqual('Hello World!');
+	});
+
+	it('should contain World word', function() {
+		expect(text).toContain('World');
 	});
 });
